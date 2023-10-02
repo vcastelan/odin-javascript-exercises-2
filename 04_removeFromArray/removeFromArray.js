831d106cb06959dda999e03b3ca09dafb85c8262
Urvino <castelanu91@gmail.com>
2023-10-02 15:37:38 -0700

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  //i: array and some other argument(s)
+  //o: array with some arguments removed/filtered out.
+  return array.filter(ele => !args.includes(ele))
 };
 
 // Do not edit below this line
