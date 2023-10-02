72dfbbd6a388fa5dd7a6a962055d8e5c18582a09
Urvino <castelanu91@gmail.com>
2023-10-02 15:25:09 -0700

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  //i: string
+  //o: string in reverse.
+  //convert a string to an array to use a method to add the last ele to the front.
+  // OR
+  // let newStr = '';
+  // string = string.split('');
+  // while(string.length > 0) {
+  //   newStr += string.pop();
+  // }
+  // return newStr;
+  //OR
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
