64e725412383e6008b241532840110f1b7ebccf1
Urvino <castelanu91@gmail.com>
2023-10-02 16:18:33 -0700

    finsihed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+  //i: years in number format aka 1900. 
+  //o: boolean to determine whether passed in year is a leap year or not.
+  //using an if statement to check whether passed in arg is a leap year. using the modulo operator to be divisble 
+  //by 4 and not divsible by 100 or divsible by 400.
+  if(years % 4 === 0 && ((years % 100 !== 0) || years % 400 === 0)) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
