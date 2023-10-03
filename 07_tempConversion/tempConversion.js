1e0b1746cbb3dac1a82e9d56a79a56c5d8333c4c
Urvino <castelanu91@gmail.com>
2023-10-02 17:07:40 -0700

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  //i: farenheit number
+  //o: celsius number rounded to the nearest decimal
+  let celsius = 5 / 9 * (farenheit - 32) 
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //i: celsius number
+  //o: farenheit number rounded to the nearest decimal
+  let farenheit = (9 / 5 * celsius) + 32;
+  return Math.round(farenheit* 10) / 10;
 };
 
 // Do not edit below this line
