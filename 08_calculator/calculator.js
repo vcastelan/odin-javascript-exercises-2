0e630d6189d6b4f449fa96f7617eead22f767c71
Urvino <vino.castelan@gmail.com>
2023-10-12 17:39:17 -0700

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	//base case: if num is negative number return 1
+  if(num === 0) {
+    return 1;
+  }
+  return num * (factorial(num - 1));
 };
 
 // Do not edit below this line
