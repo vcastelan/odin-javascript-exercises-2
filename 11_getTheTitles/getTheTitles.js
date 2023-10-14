f94a9351ece4c964563110dc47585b8e6bb7d8c3
Urvino <vino.castelan@gmail.com>
2023-10-14 13:26:57 -0700

    finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  //i: array of book objects
+  //o: an array of book titles
+  //use an array methods. aka filter
+  return books.map(ele => ele.title);
 };
 
 // Do not edit below this line
