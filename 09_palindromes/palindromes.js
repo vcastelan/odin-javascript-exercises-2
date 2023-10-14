b2a5afca0c6489c2dd27a7b53b48c640e96fa018
Urvino <vino.castelan@gmail.com>
2023-10-14 13:01:29 -0700

    finished palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // i: word
+  // o: boolean, word is spelled same backwards and forwards
+  //use recursion
+  //base case:
+  //if the word length is only 1 letter return true
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
